24e544f8a8015c11e0ae3682ef58b2f073a9f47e
Blamcho <94477654+Blamcho@users.noreply.github.com>
2025-10-02 02:26:16 -0600

    timer implementación

--- Assets/[SCRIPTS]/Sistemas/Timer.cs
+++ Assets/[SCRIPTS]/Sistemas/Timer.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TMP_Text timerText;
+    public float timerLap;
+    private int  minutes, seconds, cents;
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        timerLap += Time.fixedDeltaTime;
+        minutes = (int)(timerLap / 60f);
+        seconds = (int)(timerLap - minutes * 60f);
+        cents = (int)((timerLap - (int)timerLap) * 100f);
+      
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, cents);
     }
-}
+} 
