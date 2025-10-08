f0241edff9a3faf0abc4d13bc83878d47291f8d2
lordvlads77 <zap363@amerike.edu.mx>
2025-10-08 13:18:33 -0600

    Started Movement Revamp

--- Assets/[SCRIPTS]/Jugador/TopDownMovement.cs
+++ Assets/[SCRIPTS]/Jugador/TopDownMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -7,22 +8,55 @@ using UnityEngine.Serialization;
 
 public class TopDownMovement : MonoBehaviour
 {
+    public static TopDownMovement Instance { get; private set; }
     [FormerlySerializedAs("accelarationF")] [SerializeField] private float _accelarationF = default;
     [SerializeField] private float _turn = default;
     
     [FormerlySerializedAs("accelaration")] [SerializeField] private float _accelaration = default;
     [SerializeField] private float _steering = default;
 
-    private Rigidbody2D _elrigido;
+    [SerializeField] private float _rotationAngle = default;
+    
+    [SerializeField] private Rigidbody2D _elrigido;
 
-    void Start()
+    private void Awake()
     {
+        Instance = this;
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovementForce();
+        ApplySteerinng();
+    }
+
+    void ApplyMovementForce()
+    {
+        Vector2 engineForceVector = transform.up * _accelaration * _accelarationF;
+        
+        _elrigido.AddForce(engineForceVector, ForceMode2D.Force);
+    }
+
+    void ApplySteerinng()
+    {
+        _rotationAngle -= _steering * _turn;
+        
+        _elrigido.MoveRotation(_rotationAngle);
+    }
+
+    void MurderOrthogonalVelocity()
+    {
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(_elrigido.velocity, transform.up);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetInputVector(Vector2 inputVector)
     {
-        
+        _steering = inputVector.x;
+        _accelaration = inputVector.y;
     }
 }
