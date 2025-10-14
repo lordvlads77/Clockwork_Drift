eee345005500559e4def2bc634d5a73cbee735a0
Eva Luna Álvarez <zap363@amerike.edu.mx>
2025-10-14 13:54:53 -0600

    LapSystem Done | Programming Wise

---
+++ Assets/[SCRIPTS]/Sistemas/GameManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    [SerializeField] private TextMeshProUGUI lapText;
+    [SerializeField] private int _totalLaps = default;
+    [SerializeField] private int _currentLap = default;
+
+    private void Awake()
+    {
+        Instance = this;
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void IncrementLap()
+    {
+        _currentLap++;
+        lapText.text = "Lap: " + _currentLap + "/" + _totalLaps;
+        if (_currentLap >= _totalLaps)
+        {
+            //Win Event here
+            Debug.Log("Finalizaste la carrera!");
+        }
+    }
+}
